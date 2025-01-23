81b8ba28260fc317b94fd16965a3ecebabf9d596
Dan <cartwrightdan15@googlemail.com>
2025-01-23 20:42:22 +0000

    basically recode the entire thing so far..

--- src/main/java/me/frxq15/boostersx/datafactory/DataFactory.java
+++ src/main/java/me/frxq15/boostersx/datafactory/DataFactory.java
@@ -1,4 +1,32 @@
 package me.frxq15.boostersx.datafactory;
 
-public class DataFactory {
+import me.frxq15.boostersx.object.Booster;
+import me.frxq15.boostersx.object.GPlayer;
+import me.frxq15.boostersx.object.PlayerBoost;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public interface DataFactory {
+
+    // The main interface for the data factory
+    boolean initialize();
+    void terminate();
+    int startSavingTask();
+
+    // Player methods
+    void updatePlayerName(UUID uuid, String name);
+    void initializePlayerData(GPlayer gPlayer);
+    void getGPlayerData(UUID uuid);
+    void updateGPlayerData(GPlayer gPlayer);
+    boolean doesPlayerExist(UUID uuid);
+    boolean isPlayerDataLoaded(UUID uuid);
+    void loadPlayerData(UUID uuid);
+    void unloadPlayerData(UUID uuid);
+    Map<UUID, GPlayer> getPlayers();
+
+    // Booster methods
+    List<Booster> getBoosters(UUID uuid);
+    List<PlayerBoost> getActiveBoosts(UUID uuid);
 }
--- src/main/java/me/frxq15/boostersx/manager/LocaleManager.java
+++ src/main/java/me/frxq15/boostersx/manager/LocaleManager.java
@@ -14,6 +14,9 @@ public class LocaleManager {
     public void sendMessage(CommandSender player, String path) {
         player.sendMessage(plugin.colourize(plugin.getConfig().getString("MESSAGES." + path)));
     }
+    public void sendRawMessage(CommandSender player, String message) {
+        player.sendMessage(plugin.colourize(message));
+    }
     public void sendListMessage(CommandSender player, String path) {
         plugin.getConfig().getStringList("MESSAGES." + path).forEach(line -> {
             player.sendMessage(plugin.colourize(line));
